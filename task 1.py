0136cf9b1ffb78276d04e046b078522916aaea23
Hope141 <nadiaoussatchova@gmail.com>
2025-01-12 14:48:17 +0200

    task 1

--- task 1.py
+++ task 1.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def get_days_from_today(date):
     try:
         string_date = datetime.strptime(date, '%Y-%m-%d').date()
-        current_day = datetime.today()
+        current_day = datetime.today().date()
         return (current_day - string_date).days
     except ValueError:
         raise ValueError("Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'.")
