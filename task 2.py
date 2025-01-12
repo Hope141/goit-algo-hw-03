66aa3d4e3a24e63892969d1002a5d0a9e30a8495
Hope141 <nadiaoussatchova@gmail.com>
2025-01-12 15:30:31 +0200

    task 2

---
+++ task 2.py
@@ -0,0 +1,12 @@
+from random import randint
+
+def get_numbers_ticket(min, max, quantity):
+    result = set()
+    while len(result) < quantity:
+        result.add(randint(min, max))
+        numbers = sorted(list(result))
+    return numbers
+
+
+lottery_numbers = get_numbers_ticket(1, 100, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
